0c95c38c5882c577ad8e02eb22591167ef1d4081
abhishekkumar <abhishekkumar@localhost>
2022-09-03 13:57:06 +0530

    Commit 2

--- Automation/src/test/java/Cucumber/Automation/AppTest.java
+++ Automation/src/test/java/Cucumber/Automation/AppTest.java
@@ -4,17 +4,12 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	public static void main(String[] args)
+	{
+		System.out.print("Hello world");
+	}
+	
+	
 }
