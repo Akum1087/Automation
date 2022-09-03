7a276565fb24193aa9f48dc91506480d7b1a66bb
abhishekkumar <abhishekkumar@localhost>
2022-09-03 14:08:31 +0530

    Commit 3

--- Automation/src/test/java/Cucumber/Automation/AppTest.java
+++ Automation/src/test/java/Cucumber/Automation/AppTest.java
@@ -9,6 +9,8 @@ public class AppTest
 	public static void main(String[] args)
 	{
 		System.out.print("Hello world");
+		System.out.print("Hello world2");
+		System.out.print("Hello world3");
 	}
 	
 	
